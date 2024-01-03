cddbf5f40e835c82a5f812dee9e5fdaa52231ed7
YassineBV <bussinessarto@gmail.com>
2024-01-03 17:59:21 +0100

    task

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 for asci in range(122, 96, -1):
-    print("{}".format(chr(asci)) if asci % 2 == 0 else "{}".format(chr(asci - 32)), end="")
+    print(
+        "{}".format(chr(asci)) if asci % 2 == 0 
+        else "{}".format(chr(asci - 32)), end=""
+        )
 
