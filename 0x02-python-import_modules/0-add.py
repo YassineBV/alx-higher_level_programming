973f761ce4039319c3fdc585d2208fe0b2de713a
YassineBV <bussinessarto@gmail.com>
2024-01-04 14:04:28 +0100

    task

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+
 from add_0 import add
-nmAdd = add(
-    a= 1,
-    b= 2
-)
+
+a= 1
+b= 2
+
+
 if __name__ == "__main__":
-    print("{} + {} = {}".format(1, 2, nmAdd))
+    nmAdd  = add(a=a, b=b)
+    print("{} + {} = {}".format(a, b, nmAdd))
