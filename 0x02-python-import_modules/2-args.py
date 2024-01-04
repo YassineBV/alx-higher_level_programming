035e771732fc6be2596c759ddda035af828f8ca2
YassineBV <bussinessarto@gmail.com>
2024-01-04 17:45:27 +0100

    task

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,10 +4,11 @@ argvlent = len(argv)
 if __name__ == "__main__":
     if argvlent == 1:
         print("{} arguments.".format(argvlent - 1))
-    elif argvlent == 2:
-        print("{} argument.".format(argvlent - 1))
-        print("{}: {}".format(1, argv[1]))
-    elif len(argv) > 2:
-        print("{} arguments.".format(len(argv) - 1))
-        for i in range(1, argvlent):
-            print("{}: {}".format(i, argv[i]))
+    elif argvlent > 1:
+        if argvlent == 2:
+            print("{} argument:".format(argvlent - 1))
+            print("{}: {}".format(1, argv[1]))
+        else:
+            print("{} arguments:".format(argvlent - 1))
+            for i in range(1, argvlent):
+                print("{}: {}".format(i, argv[i]))
