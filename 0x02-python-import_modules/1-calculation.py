f07157301958d2f238de6224e37c084f4376f00f
YassineBV <bussinessarto@gmail.com>
2024-01-04 15:07:34 +0100

    task

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, div, mul
+a = 10
+b = 5
+if __name__ == "__main__":
+    adreslt = add(a=a, b=b)
+    subreslt = sub(a=a, b=b)
+    divreslt = div(a=a, b=b)
+    mulreslt = mul(a=a, b=b)
+    print("{} + {} = {}".format(a, b, adreslt))
+    print("{} - {} = {}".format(a, b, subreslt))
+    print("{} / {} = {}".format(a, b, divreslt))
+    print("{} * {} = {}".format(a, b, mulreslt))
