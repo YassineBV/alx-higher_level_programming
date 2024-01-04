aada16ac8b435a4f10ecc15c0d262d8678269a25
YassineBV <bussinessarto@gmail.com>
2024-01-04 13:45:00 +0100

    task

--- 7-islower.py
+++ 7-islower.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    if ord(c) in range(97,123):
+    if ord(c) in range(97, 123):
         return True
-    elif ord(c) in range(65,90):
+    elif ord(c) in range(65, 90):
         return False
