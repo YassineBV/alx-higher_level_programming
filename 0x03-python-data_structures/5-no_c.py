48eca3a078793548bfabb74e5fb54a9d1f654da3
YassineBV <bussinessarto@gmail.com>
2024-01-07 16:55:26 +0100

    task

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+def no_c(my_string):
+    my_new = ''
+    for char in my_string:
+        if char == 'c' or char == 'C':
+            continue
+        my_new += char
+
+    return my_new
