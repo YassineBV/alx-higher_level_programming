b17c0a80d5de6b39f482e0778b198e938f6f4701
YassineBV <bussinessarto@gmail.com>
2024-01-08 14:30:55 +0100

    task

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,7 +2,7 @@
 def divisible_by_2(my_list=[]):
     my_new = []
 
-    for intg in my_list[:len(my_list) - 1]:
+    for intg in my_list:
         if intg % 2 == 0:
             my_new.append(True)
         else:
