e40eab126369cfdab401a9b96504aa93b6c442c6
YassineBV <bussinessarto@gmail.com>
2024-01-08 11:08:13 +0100

    task

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for line in matrix:
-        for  numbr in range(len(line)):
+        for numbr in range(len(line)):
             if numbr == (len(line) - 1):
                 print("{:d}".format(line[numbr]), end="")
             else:
