2f0debcb46af979134975e57f6731cd7d953aca6
YassineBV <bussinessarto@gmail.com>
2024-01-08 14:28:49 +0100

    task

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    my_new = []
+
+    for intg in my_list[:len(my_list) - 1]:
+        if intg % 2 == 0:
+            my_new.append(True)
+        else:
+            my_new.append(False)
+    return my_new
