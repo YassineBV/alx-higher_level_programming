a6ad4872f57bee2383e3739683be0bccb9c9871d
YassineBV <bussinessarto@gmail.com>
2024-01-22 14:50:49 +0100

    task

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,5 +3,7 @@ def safe_print_integer(value):
     try:
         print("{:d}".format(value), end="\n")
         return True
-    except:
+    except ValueError:
+        return False
+    except TypeError:
         return False
