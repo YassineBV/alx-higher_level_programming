e4ded8e2b60a54897c8030e5b1c94ec457a7eb2e
YassineBV <bussinessarto@gmail.com>
2024-01-24 12:20:41 +0100

    task

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -8,6 +8,7 @@ class Square:
     def __init__(self, size=0):
         try:
             self._Square__size = size
+        
             if size < 0:
                 raise ValueError("size must be >= 0")
 
