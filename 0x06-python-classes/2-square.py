30e950cdbf5c28e8c376e6803f2e5667ce4fd229
YassineBV <bussinessarto@gmail.com>
2024-01-24 12:36:18 +0100

    task

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 """Square that defines a square"""
+
+
 class Square:
     """
     Private instance attribute: size
     Instantiation with optional size: def __init__(self, size=0)
     """
     def __init__(self, size=0):
-        try:
-            self._Square__size = size
-        
-            if size < 0:
-                raise ValueError("size must be >= 0")
-
-        except TypeError:
-            print("size must be an integer")
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
