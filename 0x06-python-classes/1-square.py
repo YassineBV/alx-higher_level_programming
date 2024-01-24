9a9851c43eff406b18f0f49655956fceba5fecf3
YassineBV <bussinessarto@gmail.com>
2024-01-24 11:39:06 +0100

    task

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Write a class Square that defines a square by: (based on 0-square.py)
+
+    Private instance attribute: size
+    Instantiation with size (no type/value verification)
+"""
+
+
+class Square:
+    """This class represents a square shape.
+    """
+    def __init__(self, size):
+        self._Square__size = size
