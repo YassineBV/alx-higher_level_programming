a2ce09a714afa800562a79562c30402cc4029404
YassineBV <bussinessarto@gmail.com>
2024-01-25 17:07:44 +0100

    task

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 import sys
+
+
 def safe_print_integer_err(value):
     bool = None
     try:
         print("{:d}".format(value))
         bool = True
     except Exception as message:
-        bool =  False
+        bool = False
         print("Exception: {}".format(message), file=sys.stderr)
     finally:
         return bool
